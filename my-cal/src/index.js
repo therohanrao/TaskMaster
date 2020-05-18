21ffe8dc0902b90d704cb084e542c9579a738b90
brianchap <brianchap@protonmail.com>
2020-05-17 21:11:23 -0700

    Go to localhost:8000

--- my-cal/src/index.js
+++ my-cal/src/index.js
@@ -1,63 +1,21 @@
-class Square extends React.Component {
-    render() {
-	return (
-      <button className="square">
-	  {/* TODO */}
-      </button>
-		);
-    }
-}
 
-class Board extends React.Component {
-    renderSquare(i) {
-	return <Square />;
-    }
 
-    render() {
-	const status = 'Next player: X';
-
-	return (
-      <div>
-      <div className="status">{status}</div>
-        <div className="board-row">
-          {this.renderSquare(0)}
-          {this.renderSquare(1)}
-          {this.renderSquare(2)}
-        </div>
-        <div className="board-row">
-	    {this.renderSquare(3)}
-	    {this.renderSquare(4)}
-	    {this.renderSquare(5)}
-        </div>
-        <div className="board-row">
-	    {this.renderSquare(6)}
-	    {this.renderSquare(7)}
-	    {this.renderSquare(8)}
-        </div>
-      </div>
-		);
-    }
-}
-
-class Game extends React.Component {
-    render() {
-	return (
-      <div className="game">
-        <div className="game-board">
-          <Board />
-        </div>
-        <div className="game-info">
-      <div>{/* status */}</div>
-      <ol>{/* TODO */}</ol>
-        </div>
-      </div>
-		);
-    }
-}
-
-// ========================================
-
-ReactDOM.render(
-		<Game />,
-		document.getElementById('root')
-		);
+let http = require('http');
+let fs = require('fs');
+ 
+let handleRequest = (request, response) => {
+    response.writeHead(200, {
+        'Content-Type': 'text/html'
+    });
+    fs.readFile('./index.html', null, function (error, data) {
+        if (error) {
+            response.writeHead(404);
+            response.write('Whoops! File not found!');
+        } else {
+            response.write(data);
+        }
+        response.end();
+    });
+};
+ 
+http.createServer(handleRequest).listen(8000);
