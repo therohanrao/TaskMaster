cd4d55885e58225daedf54b80d72c8ed1a885fdd
brianchap <brianchap@protonmail.com>
2020-05-20 13:56:56 -0700

    Fully functional links

--- my-cal/src/index.js
+++ my-cal/src/index.js
@@ -1,21 +1,33 @@
-
-
 let http = require('http');
 let fs = require('fs');
- 
+let path = require('path');
+
 let handleRequest = (request, response) => {
     response.writeHead(200, {
         'Content-Type': 'text/html'
     });
-    fs.readFile('./index.html', null, function (error, data) {
-        if (error) {
-            response.writeHead(404);
-            response.write('Whoops! File not found!');
-        } else {
-            response.write(data);
-        }
-        response.end();
-    });
+    if(request.url.match("\.html$")){
+        fs.readFile('.' + request.url, null, function (error, data) {
+            if (error) {
+                response.writeHead(404);
+                response.write(request.url);
+                response.write('Whoops! File not found!');
+            } else {
+                response.write(data);
+            }
+            response.end();
+        });
+    } else {
+        fs.readFile('./index.html', null, function (error, data) {
+            if (error) {
+                response.writeHead(404);
+                response.write('Whoops! File not found!');
+            } else {
+                response.write(data);
+            }
+            response.end();
+        });
+    }
 };
- 
+
 http.createServer(handleRequest).listen(8000);
