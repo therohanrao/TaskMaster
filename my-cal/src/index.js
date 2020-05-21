25205cd03e4404f85fa75ac292eed6a0537b2ba0
brianchap <brianchap@protonmail.com>
2020-05-20 22:13:29 -0700

    Fixed npm start bug and enabled server-client communication.

--- my-cal/src/index.js
+++ my-cal/src/index.js
@@ -1,13 +1,14 @@
 let http = require('http');
 let fs = require('fs');
 let path = require('path');
+const mysql = require('mysql');
 
 let handleRequest = (request, response) => {
     response.writeHead(200, {
         'Content-Type': 'text/html'
     });
     if(request.url.match("\.html$")){
-        fs.readFile('.' + request.url, null, function (error, data) {
+        fs.readFile('./src' + request.url, null, function (error, data) {
             if (error) {
                 response.writeHead(404);
                 response.write(request.url);
@@ -17,8 +18,19 @@ let handleRequest = (request, response) => {
             }
             response.end();
         });
+    } else if (request.url.match("\.php$")){
+        fs.readFile('./src' + request.url, null, function (error, data) {
+            if (error) {
+                response.writeHead(404);
+                response.write(request.url);
+                response.write('Whoops! File not found!');
+            } else {
+                response.write(data);
+            }
+            response.end();
+        });	
     } else {
-        fs.readFile('./index.html', null, function (error, data) {
+        fs.readFile('./src/index.html', null, function (error, data) {
             if (error) {
                 response.writeHead(404);
                 response.write('Whoops! File not found!');
@@ -31,3 +43,26 @@ let handleRequest = (request, response) => {
 };
 
 http.createServer(handleRequest).listen(8000);
+
+const connection = mysql.createConnection({
+    host: "localhost",
+    user: "root",
+    password: "",
+    database : "users",
+    port: 3306
+});
+
+connection.connect(function(err) {
+    if(err) {
+        console.log("An error occurred with connection.");
+    }
+    console.log("Connected!");
+});
+
+connection.query("INSERT INTO users (em, agreement, sq, sa, un, pw) VALUES ('brianchap@protonmail.com', '1', 'Who is the Eggman?', 'Eggert', 'Brian', 'Brian')", function(err, result) {
+    if (err) {
+        console.log("An error occurred performing the query.");
+        return;
+    }
+    console.log("Query successfully executed");
+});
