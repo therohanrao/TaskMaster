f6b7d27ce42212635f9d67ce5033045cc418dae8
brianchap <brianchap@protonmail.com>
2020-05-28 21:55:06 -0700

    Contribute, delete, and forgot passwords.

--- my-cal/src/index.js
+++ my-cal/src/index.js
@@ -8,6 +8,10 @@ const bodyParser = require('body-parser');
 
 var app = express();
 var jsonParser = bodyParser.json();
+var usernameValid = null;
+var username100 = null;
+global.securityQuestionValid = null;
+app.engine('html', require('ejs').renderFile);
 
 app.get('/', function(req, res) {
     console.log('Called 1.');
@@ -26,22 +30,64 @@ app.get('/create', function(req, res) {
 
 app.get('/calendar', function(req, res) {
     console.log('Called 4.');
-    res.sendFile(path.join(__dirname + '/calendar.html'));
+    if (usernameValid != null) {
+        res.sendFile(path.join(__dirname + '/calendar.html'));
+    }
 });
 
 app.get('/taskadd', function(req, res) {
     console.log('Called 5.');
-    res.sendFile(path.join(__dirname + '/taskadd.html'));
+    if (usernameValid != null) {
+        res.sendFile(path.join(__dirname + '/taskadd.html'));
+    }
 });
 
 app.get('/taskfind', function(req, res) {
     console.log('Called 6.');
-    res.sendFile(path.join(__dirname + '/taskfind.html'));
+    if (usernameValid != null) {
+        res.sendFile(path.join(__dirname + '/taskfind.html'));
+    }
 });
 
 app.get('/tasklist', function(req, res) {
     console.log('Called 7.');
-    res.sendFile(path.join(__dirname + '/task-list.html'));
+    if (usernameValid != null) {
+        res.sendFile(path.join(__dirname + '/task-list.html'));
+    }
+});
+
+app.get('/forgot', function(req, res) {
+    console.log('Called 8.');
+    res.sendFile(path.join(__dirname + '/forgot.html'));
+});
+
+app.get('/forgot2', function(req, res) {
+    console.log('Called 9.');
+    res.sendFile(path.join(__dirname + '/securityq.html'));
+});
+
+app.get('/contact', function(req, res) {
+    console.log('Called 10.');
+    res.sendFile(path.join(__dirname + '/contact.html'));
+});
+
+app.get('/taskcontribute', function(req, res) {
+    console.log('Called 11.');
+    if (usernameValid != null) {
+        res.sendFile(path.join(__dirname + '/contribute.html'));
+    }
+});
+
+app.get('/taskdelete', function(req, res) {
+    console.log('Called 12.');
+    if (usernameValid != null) {
+        res.sendFile(path.join(__dirname + '/delete.html'));
+    }
+});
+
+app.post('/escape', function(req, res) {
+    console.log('Called 13.');
+    res.sendFile(path.join(__dirname + '/index.html'));
 });
 
 var dbConfig = mysql.createConnection({
@@ -49,7 +95,7 @@ var dbConfig = mysql.createConnection({
     user: 'root',
     password: '',
     database: 'users',
-    port: '3306'
+    port: '3308'
 });
 
 var dbConfig2 = mysql.createConnection({
@@ -57,7 +103,7 @@ var dbConfig2 = mysql.createConnection({
     user: 'root',
     password: '',
     database: 'tasks',
-    port: '3306'
+    port: '3308'
 });
 
 dbConfig.connect((err) => {
@@ -82,10 +128,43 @@ app.use(express.static(path.join(__dirname + '/create-account.html')));
 app.use(express.static(path.join(__dirname + '/index.html')));
 app.use(express.static(path.join(__dirname + '/taskadd.html')));
 app.use(express.static(path.join(__dirname + '/taskfind.html')));
+app.use(express.static(path.join(__dirname + '/contribute.html')));
+app.use(express.static(path.join(__dirname + '/delete.html')));
+app.use(express.static(path.join(__dirname + '/securityq.html')));
+app.use(express.static(path.join(__dirname + '/newpass.html')));
 app.use(express.static('public'));
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 app.use(morgan('short'));
+
+function getToLeaf(idno) {
+    dbConfig2.query("SELECT * FROM tasks WHERE taskid=? AND author=?;", [idno, usernameValid], (err, rows, fields)=>{
+        if (rows[0].subtask == null) {
+            var previd = rows[0].taskid;
+            var newid = null;
+            var prevname = rows[0].name;
+            var prevdescription = rows[0].description;
+            var prevauthor = rows[0].author;
+            dbConfig2.query("INSERT INTO tasks (name, description, author, contributor) VALUES (?, ?, ?, ?);", [prevname, prevdescription, prevauthor, usernameValid], (err, rows, fields)=>{
+                if (!err) {
+                    console.log(rows.insertId);
+                    var newid = rows.insertId;
+                    dbConfig2.query("UPDATE tasks SET subtask=? WHERE taskid=?;", [newid, previd], (err, rows, fields)=>{
+                        if (!err) {
+                            return 0;
+                        } else {
+                            console.log(err);
+                        }
+                    });
+                } else {
+                    console.log(err);
+                }
+            });
+        } else {
+            return getToLeaf(rows[0].subtask);
+        }
+    });
+}
 
 app.post('/addUser', function(request, response) {
     var em2 = request.body.em;
@@ -93,6 +172,7 @@ app.post('/addUser', function(request, response) {
     var sa2 = request.body.sa;
     var un2 = request.body.username;
     var pwd2 = request.body.pwd;
+    var pwd3 = request.body.pwd2;
     dbConfig.query("INSERT INTO users (em, agreement, sq, sa, un, pw) VALUES ( ?, '1', ?, ?, ?, ?);", [em2, sq2, sa2, un2, pwd2], (err, rows, fields)=>{
         if(!err) {
             console.log(rows);
@@ -108,6 +188,7 @@ app.post('/login', function(request, response) {
     var pwd2 = request.body.password;
     dbConfig.query("SELECT * FROM users WHERE un = ? AND pw = ?", [un2, pwd2], (err, rows, fields)=>{
         if (rows.length > 0) {
+            usernameValid = un2;
             return response.redirect('/calendar');
         } else {
             console.log('Invalid credentials.');
@@ -119,7 +200,7 @@ app.post('/login', function(request, response) {
 app.post('/addTask', function(request, response) {
     var na2 = request.body.name;
     var de2 = request.body.description;
-    dbConfig2.query("INSERT INTO tasks (name, description) VALUES (?, ?);", [na2, de2], (err, rows, fields)=>{
+    dbConfig2.query("INSERT INTO tasks (name, description, author) VALUES (?, ?, ?);", [na2, de2, usernameValid], (err, rows, fields)=>{
         if(!err) {
             console.log(rows);
         } else {
@@ -153,10 +234,94 @@ app.post('/findTask2', function(request, response) {
     });
 });
 
+app.post('/contributeTask', function(request, response) {
+    var idno = request.body.id;
+    dbConfig2.query("SELECT * FROM tasks WHERE taskid=? AND author=?;", [idno, usernameValid], (err, rows, fields)=>{
+        if (rows.length > 0) {
+            if (rows[0].contributor == null) {
+                dbConfig2.query("UPDATE tasks SET contributor=? WHERE taskid=?;", [usernameValid, idno], (err, rows, fields)=>{
+                    if (!err) {
+                        return response.redirect('/calendar');
+                    } else {
+                        console.log(err);
+                    }
+                });                
+            } else {
+                var dummy = getToLeaf(idno);
+                return response.redirect('/calendar');
+            }
+        } else {
+            console.log('No task identified.');
+        }
+    });
+});
+
+app.post('/deleteTask', function(request, response) {
+    var idno = request.body.id;
+    dbConfig2.query("SELECT * FROM tasks WHERE taskid=? AND author=?;", [idno, usernameValid], (err, rows, fields)=>{
+        if (rows.length > 0) {
+            var tempid = rows[0].taskid;
+            var temp = rows[0].subtask;
+            if (temp != null) {
+                dbConfig2.query("SELECT * from tasks WHERE subtask=?;", [tempid], (err, rows, fields)=>{
+                    if (rows.length > 0) {
+                        var newid = rows[0].taskid;
+                        dbConfig2.query("UPDATE tasks SET subtask=? WHERE taskid=?;", [temp, newid], (err, rows, fields)=>{
+                            if (err) {
+                                console.log(err);
+                            }
+                        });
+                    }
+                });
+            }
+            dbConfig2.query("DELETE FROM tasks WHERE taskid=?;", [idno], (err, rows, fields)=>{
+                if (!err) {
+                    return response.redirect('/calendar');
+                } else {
+                    console.log(err);
+                }
+            });
+        } else {
+            console.log('No task identified.');
+        }
+    });
+});
+
+app.post('/forgot2', function(request, response) {
+    username100 = request.body.username;
+    dbConfig.query("SELECT * FROM users WHERE un=?", [username100], (err, rows, fields)=>{
+        if(rows.length > 0) {       
+            securityQuestionValid = rows[0].sq;
+            console.log(securityQuestionValid);
+            return response.render(path.join(__dirname + '/securityq.html'), {name:securityQuestionValid});
+        } else {
+            console.log(err);
+        }
+    });
+});
+
+app.post('/newpass', function(request, response) {
+    var answerQuestion = request.body.answer;
+    dbConfig.query("SELECT * FROM users WHERE un=? AND sa=?", [username100, answerQuestion], (err, rows, fields)=>{
+        if (rows.length > 0) {
+            var password = rows[0].pw;
+            return response.render(path.join(__dirname + '/newpass.html'), {name:password});
+        }
+    });
+});
+
 app.post('/redirect1', function(request, response) {
     return response.redirect('/taskadd');
 });
 
 app.post('/redirect2', function(request, response) {
     return response.redirect('/taskfind');
+});
+
+app.post('/redirect3', function(request, response) {
+    return response.redirect('/taskcontribute');
+});
+
+app.post('/redirect4', function(request, response) {
+    return response.redirect('/taskdelete');
 });
