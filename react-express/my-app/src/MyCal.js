ccc3115e5d3a87d898c84624f1f0043ea2924261
therohanrao <59488758+therohanrao@users.noreply.github.com>
2020-05-31 23:09:51 -0700

    Added Navbar to Calendar

--- react-express/my-app/src/MyCal.js
+++ react-express/my-app/src/MyCal.js
@@ -1,6 +1,64 @@
-import React, { Component } from 'react';
+import React, { Component, useState } from 'react';
 import Calendar from 'react-calendar';
 import 'react-calendar/dist/Calendar.css'
+import 'bootstrap/dist/css/bootstrap.css';
+import {
+  Collapse,
+  Navbar,
+  NavbarToggler,
+  NavbarBrand,
+  Nav,
+  NavItem,
+  NavLink,
+  UncontrolledDropdown,
+  DropdownToggle,
+  DropdownMenu,
+  DropdownItem,
+  NavbarText
+} from 'reactstrap';
+
+const Example = (props) => {
+  const [isOpen, setIsOpen] = useState(false);
+
+  const toggle = () => setIsOpen(!isOpen);
+
+  return (
+    <div>
+      <Navbar color="light" light expand="md">
+        <NavbarBrand href="/">reactstrap</NavbarBrand>
+        <NavbarToggler onClick={toggle} />
+        <Collapse isOpen={isOpen} navbar>
+          <Nav className="mr-auto" navbar>
+            <NavItem>
+              <NavLink href="/components/">Components</NavLink>
+            </NavItem>
+            <NavItem>
+              <NavLink href="https://github.com/reactstrap/reactstrap">GitHub</NavLink>
+            </NavItem>
+            <UncontrolledDropdown nav inNavbar>
+              <DropdownToggle nav caret>
+                Options
+              </DropdownToggle>
+              <DropdownMenu right>
+                <DropdownItem>
+                  Option 1
+                </DropdownItem>
+                <DropdownItem>
+                  Option 2
+                </DropdownItem>
+                <DropdownItem divider />
+                <DropdownItem>
+                  Reset
+                </DropdownItem>
+              </DropdownMenu>
+            </UncontrolledDropdown>
+          </Nav>
+          <NavbarText>Simple Text</NavbarText>
+        </Collapse>
+      </Navbar>
+    </div>
+  );
+}
 
 class MyCal extends Component {
   state = {
@@ -19,6 +77,8 @@ class MyCal extends Component {
             height: '70vh'
         }}
       >
+
+
         <Calendar
           onChange={this.onChange}
           value={this.state.date}
@@ -28,4 +88,14 @@ class MyCal extends Component {
   }
 }
 
-export default MyCal;
+function NavCal() {
+  return(
+    <div>
+      <Example/>
+        <MyCal/>
+    </div>
+
+  );
+}
+
+export default NavCal
