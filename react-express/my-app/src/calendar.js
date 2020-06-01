ea5922fd2709440c681512603f38fdcc488e8c45
Ethan Ngo <ethan.ngo2019@gmail.com>
2020-05-31 21:31:53 -0700

    Added a React calendar that is dynamic

---
+++ react-express/my-app/src/calendar.js
@@ -0,0 +1,31 @@
+import React, { Component } from 'react';
+import Calendar from 'react-calendar';
+import 'react-calendar/dist/Calendar.css'
+ 
+class MyCal extends Component {
+  state = {
+    date: new Date(),
+  }
+ 
+  onChange = date => this.setState({ date })
+ 
+  render() {
+    return (
+      <div
+        style={{
+            display: "flex",
+            justifyContent: "center",
+            alignItems: "center",
+            height: '70vh'
+        }}
+      >
+        <Calendar
+          onChange={this.onChange}
+          value={this.state.date}
+        />
+      </div>
+    );
+  }
+}
+
+export default MyCal;
