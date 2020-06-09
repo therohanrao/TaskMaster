e8ff9080d8d140665f16287f2db6044dfb42e9be
JR Bronkar <jrbronkar@jrs-macbook-air.home>
2020-06-08 22:55:53 -0700

    Bugfix on searching a task

--- react-express/api/routes/findTask.js
+++ react-express/api/routes/findTask.js
@@ -15,7 +15,7 @@ var dbConfig = mysql.createConnection({
 router.post('/', function(request, response) {
 	var na2 = request.body.taskname;
 	console.log(na2);
-	dbConfig.query("SELECT * FROM tasks WHERE author=?;", [na2], (err, rows, fields)=>{
+	dbConfig.query("SELECT * FROM tasks WHERE name=?;", [na2], (err, rows, fields)=>{
 		if(!err) {
 		    response.render('task-list.ejs', {page_title:"Tasks", data:rows});
 		} else {
