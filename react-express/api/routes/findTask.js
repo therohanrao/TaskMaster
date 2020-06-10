53743c352829de860aa9b263e22f714ff899c8b8
JR Bronkar <jrbronkar@jrs-macbook-air.home>
2020-06-09 19:07:17 -0700

    Find Task is now specialized to Users

--- react-express/api/routes/findTask.js
+++ react-express/api/routes/findTask.js
@@ -1,5 +1,9 @@
 var express = require('express');
 var router = express.Router();
+var cookieParser = require('cookie-parser');
+var session = require('express-session');
+router.use(cookieParser());
+
 const mysql = require('mysql');
 
 
@@ -14,8 +18,9 @@ var dbConfig = mysql.createConnection({
 
 router.post('/', function(request, response) {
 	var na2 = request.body.taskname;
+	var username = request.session.username;
 	console.log(na2);
-	dbConfig.query("SELECT * FROM tasks WHERE name=?;", [na2], (err, rows, fields)=>{
+	dbConfig.query("SELECT * FROM tasks WHERE name=? AND author=?;", [na2,username], (err, rows, fields)=>{
 		if(!err) {
 		    response.render('task-list.ejs', {page_title:"Tasks", data:rows});
 		} else {
