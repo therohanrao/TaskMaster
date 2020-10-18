3ee8acccf600b299bae156bed6db55a90124b713
therohanrao <59488758+therohanrao@users.noreply.github.com>
2020-10-17 22:39:55 -0700

    Updates

---
+++ precache-manifest.bf507c9d5f07ccfac05bb747ccb4387a.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "64860731f78367d5698e3f8c87263712",
+    "url": "/TaskMaster/index.html"
+  },
+  {
+    "revision": "c1e37597b9012ba736fd",
+    "url": "/TaskMaster/static/css/2.9ae8384b.chunk.css"
+  },
+  {
+    "revision": "c1e37597b9012ba736fd",
+    "url": "/TaskMaster/static/js/2.307dd037.chunk.js"
+  },
+  {
+    "revision": "5e9b6f5c16b720e8a86875f97734673e",
+    "url": "/TaskMaster/static/js/2.307dd037.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d495b4ef2f16910b6497",
+    "url": "/TaskMaster/static/js/main.d1a6ddea.chunk.js"
+  },
+  {
+    "revision": "262c7f6e22250f3a1439",
+    "url": "/TaskMaster/static/js/runtime-main.16bf0578.js"
+  },
+  {
+    "revision": "c8519cc61adc0e907a219448e2454fcf",
+    "url": "/TaskMaster/static/media/JR_logo.c8519cc6.png"
+  }
+]);
